14eba6fcfcd61384cbeba9af0290e3fe7236b095
leetbadd288 <frontenddev344@gmail.com>
2024-07-30 14:55:07 +0530

    changes

--- javascript/custom.js
+++ javascript/custom.js
@@ -40,42 +40,50 @@ $(document).ready(function () {
 (function () {
     "use strict";
 
-    var cookieAlert = document.querySelector(".cookie-alert");
-    var acceptCookies = document.querySelector(".accept-cookies");
+    document.addEventListener("DOMContentLoaded", function () {
+        var cookieAlert = document.querySelector(".cookie-alert");
+        var acceptCookies = document.querySelector(".accept-cookies");
 
-    cookieAlert.offsetHeight; // Force browser to trigger reflow (https://stackoverflow.com/a/39451131)
+        if (cookieAlert && acceptCookies) {
+            // Ensure the alert height is calculated for any potential layout issues
+            cookieAlert.offsetHeight;
 
-    if (!getCookie("acceptCookies")) {
-        cookieAlert.classList.add("show");
+            if (!getCookie("acceptCookies")) {
+                cookieAlert.classList.add("show");
+            }
+
+            acceptCookies.addEventListener("click", function () {
+                setCookie("acceptCookies", true, 60);
+                cookieAlert.classList.remove("show");
+            });
+        }
+    });
+
+    // Cookie functions 
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
 
-    acceptCookies.addEventListener("click", function () {
-        setCookie("acceptCookies", true, 60);
-        cookieAlert.classList.remove("show");
-    });
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
 })();
 
-// Cookie functions 
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
+$(document).ready(function () {
+    $(".navlink li i").click(function () {
+        $(this).parent().next().slideToggle();
+    });
+});
